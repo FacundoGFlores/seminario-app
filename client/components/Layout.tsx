c8d4b3ad7087209ed3395a022f1cb256f4243a02
Javier Ocampo <ferroxido@gmail.com>
2019-12-14 16:54:17 -0300

    Home page

---
+++ client/components/Layout.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import { Layout } from "antd";
+import Navigation from "./Navigation";
+
+const { Footer, Content } = Layout;
+
+export default function LayoutDashboard(props) {
+  const { children, title } = props;
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Navigation isAuth={true} />
+      <Content
+        style={{
+          padding: "30px 50px 0 50px",
+          display: "flex",
+          justifyContent: "space-around"
+        }}
+      >
+        {children}
+      </Content>
+    </div>
+  );
+}

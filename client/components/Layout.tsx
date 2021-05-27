869cfb470c5c86ab3680dc31d30f00a54f00a98e
Javier Ocampo <ferroxido@gmail.com>
2021-05-26 21:55:00 -0300

    Fix styles

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { Container, Typography, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { orange } from "@material-ui/core/colors";
+import { grey } from "@material-ui/core/colors";
 import { Navbar } from "./Navbar";
 import React from "react";
 
@@ -12,36 +12,36 @@ interface Props {
 const useStyles = makeStyles({
   container: {
     backgroundColor: "#eee",
-    padding: "0",
-    borderRadius: "0 0 10px 10px",
     height: "100vh",
-    maxWidth: "80vw",
-    margin: "auto"
   },
   header: {},
   content: {
-    padding: "2em"
+    padding: "2em",
+    flex: 1,
+    maxWidth: '1200px',
+    margin: 'auto',
   },
   footer: {
-    background: `linear-gradient(to bottom, ${orange[500]}, ${orange[200]})`,
-    borderRadius: "0 0 10px 10px",
+    background: grey[800],
     color: "#fff",
-    padding: 5
+    padding: 5,
   }
 });
 
 function Layout({ children, title }: Props): JSX.Element {
   const classes = useStyles();
 
-  console.log("FOOOOOO");
   return (
     <Grid container className={classes.container} direction="column">
       <Navbar />
-      <Grid item className={classes.header}>
+      <Grid container className={classes.header}>
         {title && <Typography variant="h3">{title}</Typography>}
       </Grid>
-      <Grid item className={classes.content}>
+      <Grid container className={classes.content}>
         {children}
+      </Grid>
+      <Grid container direction="row-reverse" className={classes.footer}>
+        <Typography>By Dev&Coffee</Typography>
       </Grid>
     </Grid>
   );
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -3,26 +3,34 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { LoginButton } from "./LoginButton";
 import Link from "next/link";
+import { lightBlue } from "@material-ui/core/colors";
 
 const useStyles = makeStyles(theme => ({
   menuButton: {
     marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1
-  }
+    flexGrow: 1,
+    '& a': {
+      textDecoration: 'none !important',
+      color: '#fff !important',
+    }
+  },
+  navbar: {
+    backgroundColor: lightBlue[800],
+    color: '#fff',
+  },
 }));
 
 function Navbar() {
   const classes = useStyles();
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.navbar}>
       <Toolbar>
         <IconButton
           edge="start"

0bf071c05973fef9dc73faac2bc49b51270eb394
Facundo Flores <flores.facundogabriel@gmail.com>
2021-06-23 10:15:10 -0300

    Touch

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ssh -oStrictHostKeyChecking=no -A connan@138.197.207.195 "cd seminario-app
-    git checkout master
-    git pull --rebase
-    docker-compose up -d" -y
+ssh -oStrictHostKeyChecking=no -A connan@138.197.207.195 "    
+    touch hello.txt"
+

97f9493d06fd24ebf18f1942a25857b775ffb3ae
Facundo Flores <flores.facundogabriel@gmail.com>
2021-06-23 12:11:43 -0300

    Deploy

---
+++ server-prisma/run-it.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "-> starting prisma migrations to postgres...\n"
+cd /usr/app/
+./wait-for-it.sh db:5432 -- echo "db online\n"
+yarn run migrate:save --name init_prod
+yarn run migrate:up
+sleep 5
+yarn run build
+yarn start
